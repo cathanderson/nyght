87f89c9f8357611cf9de2dd0d9352ba17c687daa
“Catherine <cathanders10@gmail.com>
2022-12-19 15:30:59 -0500

    signup/ogin modals styled and functional

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -2,9 +2,17 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import "./NavBar.css";
 import { logout } from "../../store/session";
+import logo from "../../assets/images/logo.png";
+import { useState } from "react";
+import { LoginModal, SignupModal } from "../../context/Modal";
+import LoginForm from "../SessionForms/LoginForm";
+import SignupForm from "../SessionForms/SignupForm";
+import x from "../../assets/icons/close.png"
 
 function NavBar() {
   const loggedIn = useSelector((state) => !!state.session.user);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
   const dispatch = useDispatch();
 
   const logoutUser = (e) => {
@@ -26,14 +34,42 @@ function NavBar() {
       );
     } else {
       return (
-        <div className="links-auth">
-          <Link id="signup-form-nav-link" className="nav-link" to={"/signup"}>
-            Signup
-          </Link>
-          <Link className="nav-link" to={"/login"}>
-            Login
-          </Link>
-        </div>
+        <>
+          <div className="links-auth">
+            <Link
+              id="signup-form-nav-link"
+              className="nav-link"
+              onClick={() => setShowSignupModal(true)}
+            >
+              Sign up
+            </Link>
+            <Link className="nav-link" onClick={() => setShowLoginModal(true)}>
+              Log in
+            </Link>
+          </div>
+          {showLoginModal && (
+            <LoginModal onClose={() => setShowLoginModal(false)}>
+              <img
+                onClick={() => setShowLoginModal(false)}
+                src={x}
+                className="form-x"
+              />
+              <h3 className="modal-title">Log in</h3>
+              <LoginForm />
+            </LoginModal>
+          )}
+          {showSignupModal && (
+            <SignupModal onClose={() => setShowSignupModal(false)}>
+              <img
+                onClick={() => setShowSignupModal(false)}
+                src={x}
+                className="form-x"
+              />
+              <h3 className="modal-title">Sign Up</h3>
+              <SignupForm />
+            </SignupModal>
+          )}
+        </>
       );
     }
   };
@@ -41,7 +77,10 @@ function NavBar() {
   return (
     <>
       <nav id="main-nav">
-        <h1 id="main-logo">Nyght</h1>
+        <Link id="main-logo-container" className="nav-link" to={"/"}>
+          {/* <img id="main-logo" src={logo}/> */}
+          <h1>Nyght</h1>
+        </Link>
         <div id="nav-links-container">{getLinks()}</div>
       </nav>
     </>
