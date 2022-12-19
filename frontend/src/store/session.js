c3dc6108aeaa32291ba0e466c5fd6775abe3b953
“Catherine <cathanders10@gmail.com>
2022-12-19 10:52:43 -0500

    complete frontend auth functionality

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -47,6 +47,12 @@ const startSession = (userInfo, route) => async (dispatch) => {
   }
 };
 
+export const getCurrentUser = () => async (dispatch) => {
+  const res = await jwtFetch("/api/users/current");
+  const user = await res.json();
+  return dispatch(receiveCurrentUser(user));
+};
+
 export const logout = () => (dispatch) => {
   localStorage.removeItem("jwtToken");
   dispatch(logoutUser());
