f996533a8814d90ecd9f945f5c6c02df74ee422d
Brian Yu <briehnyu@gmail.com>
2022-12-19 17:51:21 -0500

    Add frontend venue state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./store/store";
+import * as venueActions from "./store/venues";
+import jwtFetch from "./store/jwt";
 
 let store = configureStore({});
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.jwtFetch = jwtFetch;
+  window.venueActions = venueActions;
+}
 
 function Root() {
   return (
