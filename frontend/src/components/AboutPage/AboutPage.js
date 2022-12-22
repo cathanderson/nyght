d0d0e5147128610946b2762a156b92ee88ea99ef
“Catherine <cathanders10@gmail.com>
2022-12-22 09:41:27 -0500

    complete about page

--- frontend/src/components/AboutPage/AboutPage.js
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -1,18 +1,43 @@
 import "./AboutPage.css";
-import cath from "../../assets/images/cath.png"
+import cath from "../../assets/images/cath.png";
 import brian from "../../assets/images/brian.jpg";
 import evan from "../../assets/images/evan.jpg";
-import github from "../../assets/icons/github.png"
-import linkedin from "../../assets/icons/linkedin.png"
+import github from "../../assets/icons/github.png";
+import linkedin from "../../assets/icons/linkedin.png";
 
 function AboutPage() {
   return (
     <>
       <div className="outter-about-page-container">
         <div className="inner-about-page-container">
-          <div className="about-page-container-subheader">Instructions</div>
+          <div className="about-page-container-subheader" id="about-this-project-subheader">
+            About this project
+          </div>
           <div className="about-page-content-container">
-            <div className="about-page-instructions"></div>
+            <div className="about-page-about">
+              Nyght is a New York City plan generator fit for couples looking
+              for new date ideas and groups of friends looking for new places to
+              try in different neighborhoods across the city.
+              <br></br>
+              <br></br>
+              To create a plan, navigate to the home page, select a
+              neighborhood, whether you'd prefer an after dinner cocktail or
+              dessert, and modify the provided plan by clicking a venue and
+              toggling through available options in that category.
+              <br></br>
+              <br></br>
+              Once you're satisfied with your plan, you can hit confirm. This
+              will bring you to the itinerary's page which includes a map for
+              your plan and functionality to email your plan to yourself and
+              your friends.
+              <br></br>
+              <br></br>
+              For more information, check out our{" "}
+              <a href="https://github.com/cathanderson/nyght">
+                project repo
+              </a>{" "}
+              on Github!
+            </div>
           </div>
         </div>
         <div className="inner-about-page-container">
