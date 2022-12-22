f1475ba023eed771bdbe63c0954cc90c76fe1b47
Brian Yu <briehnyu@gmail.com>
2022-12-22 10:38:24 -0500

    More fixes

--- frontend/src/store/emails.js
+++ frontend/src/store/emails.js
@@ -2,7 +2,8 @@ import jwtFetch from "./jwt";
 
 const RECEIVE_LIST = "emails/RECEIVE_LIST";
 const REMOVE_EMAIL = "emails/REMOVE_EMAIL";
-const ADD_EMAIL = "emails/ADD_EMAIL"
+const ADD_EMAIL = "emails/ADD_EMAIL";
+const UPDATE_EMAIL = "emails/UPDATE_EMAIL";
 
 export const removeEmail = (email) => ({
   type: REMOVE_EMAIL,
@@ -16,8 +17,13 @@ export const receiveList = (list) => ({
 
 export const addEmail = (email) => ({
   type: ADD_EMAIL,
-  email
-})
+  email,
+});
+
+export const updateEmail = (email) => ({
+  type: UPDATE_EMAIL,
+  email,
+});
 
 export const getList = (state) =>
   state.emails ? Object.values(state.emails) : [];
@@ -31,7 +37,7 @@ export const fetchList = (itineraryId) => async (dispatch) => {
 export const createEmail = (itineraryId, email) => async (dispatch) => {
   const res = await jwtFetch(`/api/emails/${itineraryId}`, {
     method: "POST",
-    body: JSON.stringify({email: email}),
+    body: JSON.stringify({ email: email }),
     headers: {
       "Content-Type": "application/json",
     },
@@ -52,8 +58,22 @@ export const deleteEmail = (id) => async (dispatch) => {
   dispatch(removeEmail(data));
 };
 
+export const updateEmails = (email, newEmail) => async (dispatch) => {
+  console.log(`here: ${newEmail}`);
+  const res = await jwtFetch(`/api/emails/${email._id}`, {
+    method: "PATCH",
+    body: JSON.stringify({ email: newEmail }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  dispatch(updateEmail(data.email));
+};
+
 const emailsReducer = (state = {}, action) => {
   const newState = { ...state };
+  const keys = Object.keys(newState);
   switch (action.type) {
     case RECEIVE_LIST:
       return action.list;
@@ -62,9 +82,17 @@ const emailsReducer = (state = {}, action) => {
       newState[size] = action.email;
       return newState;
     case REMOVE_EMAIL:
-      const keys = Object.keys(newState);
-      const keyToDelete = keys.find((key) => newState[key].id === action.email.id);
+      const keyToDelete = keys.find(
+        (key) => newState[key].id === action.email.id
+      );
       delete newState[keyToDelete];
+      return newState;
+    case UPDATE_EMAIL:
+      console.log(action.email);
+      const keyToUpdate = keys.find(
+        (key) => newState[key].id === action.email.id
+      );
+      newState[keyToUpdate] = action.email;
       return newState;
     default:
       return state;
